6b688522e44eeb5490d9f01dc661f036fa801084
madhavmaheshwari0904-crypto <madhavmaheshwari0904@gmail.com>
2026-07-30 22:05:50 +0530

    Time: 3 ms (20.99%) | Memory: 12.4 MB (18.52%) - LeetSync

---
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def evenOddBit(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        s=str(bin(n))[2:]
+        s=s[::-1]
+        e=0
+        o=0
+        for i in range(len(s)):
+            if(s[i]=='1' and i%2==0):
+                e+=1
+            elif(s[i]=='1' and i%2!=0):
+                o+=1
+        return [e,o]            
