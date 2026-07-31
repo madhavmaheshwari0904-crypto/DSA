b5c6bfd11bcbee4ba3d923acf0ee41820561d20f
madhavmaheshwari0904-crypto <madhavmaheshwari0904@gmail.com>
2026-07-31 20:51:05 +0530

    Time: 63 ms (32.62%) | Memory: 15.7 MB (66.31%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.arr=nums[:]
+
+    def reset(self):
+        """
+        :rtype: List[int]
+        """
+        return self.arr
+
+    def shuffle(self):
+        """
+        :rtype: List[int]
+        """
+        ans=self.arr[:]
+        for i in range(len(ans)):
+            swap=random.randrange(i,len(ans))
+            ans[i],ans[swap]=ans[swap],ans[i]
+        return ans    
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
