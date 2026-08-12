e42ce8949debb7852046bb5165c10e7070279b9e
madhavmaheshwari0904-crypto <madhavmaheshwari0904@gmail.com>
2026-08-12 21:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (40.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int solve(int[] dp,int idx,int[]cost){
+    /*public int solve(int[] dp,int idx,int[]cost){
         if(idx<0)return 0;
         if (idx==0 || idx==1) return cost[idx];
         if(dp[idx]!=-1)return dp[idx];
         dp[idx]=cost[idx]+Math.min(solve(dp,idx-1,cost),solve(dp,idx-2,cost));
         return dp[idx];
-    } 
+    } */
     public int minCostClimbingStairs(int[] cost) {
         int []dp=new int[cost.length];
-        Arrays.fill(dp,-1);
-        dp[0]=cost[0];
-        return Math.min(solve(dp,cost.length-1,cost),solve(dp,cost.length-2,cost));
+        //Arrays.fill(dp,0);
+        for(int i=0;i<cost.length;i++){
+            if(i<2)dp[i]=cost[i];
+            else dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
     }
 }
