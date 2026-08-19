248ad44d2529182415f7d08feb292103b48c80df
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-08-19 20:45:16 +0530

    Time: 19 ms (68.67%) | Memory: 54.2 MB (64.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -8,7 +8,7 @@ class Solution {
         return solve(s,t,i-1,j);
     }*/
     //TLE TC(2^n);
-    public int solve(String s, String t,int i,int j,int[][]dp){
+    /*public int solve(String s, String t,int i,int j,int[][]dp){
         if(j<0)return 1;
         if(i<0 || j<0 ||i<j)return 0;
         if (dp[i][j] != -1) return dp[i][j];
@@ -19,14 +19,18 @@ class Solution {
             dp[i][j]=solve(s,t,i-1,j,dp);
         }
         return dp[i][j];
-    }
+    }*/
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int[][]dp=new int[n][m];
-        for(int[]row:dp){
-            Arrays.fill(row,-1);
+        int[][]dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1))dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
         }
-        return solve(s,t,n-1,m-1,dp);
+        return dp[n][m];
     }
 }
