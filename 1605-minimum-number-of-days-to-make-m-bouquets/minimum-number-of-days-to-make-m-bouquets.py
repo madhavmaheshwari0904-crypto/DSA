752de238e436a43cbf3d5dc6baf76f8d1f35f57b
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-16 20:37:34 +0530

    Time: 455 ms (60.43%) | Memory: 21.6 MB (23.38%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def fun(self,bloomDay,mid,m,k):
+    """def fun(self,bloomDay,mid,m,k):
         c=0
         bouquets=0
         for i in bloomDay:
@@ -13,7 +13,7 @@ class Solution(object):
         if(bouquets>=m):
             return True
         else:
-            return False 
+            return False """
     def minDays(self, bloomDay, m, k):
         """
         :type bloomDay: List[int]
@@ -28,9 +28,18 @@ class Solution(object):
         high=max(bloomDay)
         ans=-1
         while(low<=high):
+            c=0
+            b=0
             mid=(low+high)//2
-            total=self.fun(bloomDay,mid,m,k)
-            if(total):
+            for i in bloomDay:
+                if(i<=mid):
+                    c+=1
+                    if c==k:
+                        b+=1
+                        c=0
+                else:
+                    c=0
+            if(b>=m):
                 ans=mid
                 high=mid-1
             else:
