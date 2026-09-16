56734a26cf2ca5071dc3f56a2e1ce2f89154205f
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-16 12:17:10 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (58.83%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n1=0,n2=0;
+        int ans=0;
+        for(int i:nums){
+            ans^=i;
+        }
+        int diff=ans &(-ans);
+        for(int i:nums){
+            if ((i & diff)!=0)n1^=i;
+            else n2^=i;
+        }
+        return new int[]{n1,n2};
+    }
+}
