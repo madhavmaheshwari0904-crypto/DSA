1be01e1d621935978d9bdf4e243fea3205aca48f
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-17 23:27:30 +0530

    Time: 175 ms (75.00%) | Memory: 13 MB (25.00%) - LeetSync

--- 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.py
+++ 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.py
@@ -6,14 +6,14 @@ class Solution(object):
         """
         tasks.sort(key=lambda x:x[1])
         maxi=max(task[1] for task in tasks)
-        time=[False]*(maxi+1)
+        time=[0]*(maxi+1)
         for s,e,d in tasks:
             already=sum(time[s:e+1])
             left=d-already
             t=e
             while(left>0):
                 if not time[t]:
-                    time[t]=True
+                    time[t]=1
                     left-=1
                 t-=1
         return sum(time)
