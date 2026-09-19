1e673f469d4b4a44939caa91eab26597bf49c0ec
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-19 22:26:01 +0530

    Time: 184 ms (18.36%) | Memory: 13.9 MB (97.30%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -4,22 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        l=[]
+        ans=0
+        l=0
         sign='+'
         c=0
         for i,char in enumerate(s):
             if(char.isdigit()):
                 c=c*10+int(char)
             if (not char.isspace() and not char.isdigit()) or i == len(s) - 1:
-                if sign == '+':
-                    l.append(c)
-                elif sign == '-':
-                    l.append(-c)
+                if sign in ('+','-'):
+                    ans+=l
+                    l=c if sign=='+'else -c
                 elif sign == '*':
-                    l.append(l.pop() * c)
+                    l=l*c
                 elif sign == '/':
-                    prev = l.pop()
-                    l.append(int(float(prev) / c))
+                    l=int(float(l)/c)
                 sign = char
                 c = 0
-        return sum(l)
+        ans+=l
+        return ans
