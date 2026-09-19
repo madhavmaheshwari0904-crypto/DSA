44d834e5678926a3e0c8241cb0419fe0ed81694d
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-19 22:51:02 +0530

    Time: 170 ms (61.91%) | Memory: 12.5 MB (41.12%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,10 +1,18 @@
 class Solution(object):
+    
+    def expand(self, s, left, right):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
     def countSubstrings(self, s):
         """
         :type s: str
         :rtype: int
         """
-        def pal(s,i,j):
+        """def pal(s,i,j):
             sub= s[i:j+1]
             return sub==sub[::-1]
         c=0
@@ -12,4 +20,12 @@ class Solution(object):
             for j in range(i,len(s)):
                 if(pal(s,i,j)==True):
                     c+=1
+        return c"""
+        c = 0
+        
+        for i in range(len(s)):
+            # Odd length palindromes 
+            c += self.expand(s, i, i)
+            # Even length palindromes
+            c += self.expand(s, i, i + 1)
         return c
