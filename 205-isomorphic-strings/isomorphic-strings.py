33d85b08131ebb26b298ee0d5da674b3a5fc45b4
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-09-20 20:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         s1=set(s)
         t1=set(t)
         return len(s1)==len(t1)==len(set(zip(s,t)))
